f822432024b88aa9c20a742b02e692beb7951d26
Elena Zinchenko <7simvolow@mail.ru>
2023-08-13 04:13:02 +0300

    Решение 3 задачи Модуль 2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,32 @@
 let calculator = {
-  // ваш код
 };
+
+function read(a, b) {
+
+  this.a = a;
+  this.b = b;
+
+  return {
+    a: this.a,
+    b: this.b
+  };
+}
+
+
+function sum() {
+  return this.a + this.b;
+
+};
+
+function mul() {
+  return this.a * this.b;
+}
+
+calculator.read = read;
+calculator.sum = sum;
+calculator.mul = mul;
+
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
