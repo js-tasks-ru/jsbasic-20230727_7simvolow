affef72b03752bdb31a31f7e60cadf8f8bf49c3d
Elena Zinchenko <7simvolow@mail.ru>
2023-08-14 13:00:34 +0300

    Добавить исправление в решение 3 задачи Модуль 2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,32 +1,21 @@
 let calculator = {
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+    return {
+      a: this.a,
+      b: this.b
+    };
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
 };
-
-function read(a, b) {
-
-  this.a = a;
-  this.b = b;
-
-  return {
-    a: this.a,
-    b: this.b
-  };
-}
-
-
-function sum() {
-  return this.a + this.b;
-
-};
-
-function mul() {
-  return this.a * this.b;
-}
-
-calculator.read = read;
-calculator.sum = sum;
-calculator.mul = mul;
-
-
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
